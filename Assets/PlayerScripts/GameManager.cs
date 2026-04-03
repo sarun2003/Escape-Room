c218433117daa039655c0dc462b93d12d16b59e4
ns-112 <nps007@latech.edu>
2026-04-02 19:28:07 -0500

    some changes idk

--- Assets/PlayerScripts/GameManager.cs
+++ Assets/PlayerScripts/GameManager.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel;
 using UnityEngine;
 
 public enum PlayerState
---
+++ Assets/PlayerScripts/HUDManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class HUDManager : MonoBehaviour
+{
+    void Start() {
+        
+    }
+}
